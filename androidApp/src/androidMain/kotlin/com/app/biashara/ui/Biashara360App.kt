0cf328f266402f8c40776326273c8a57d6237e6e
pnjunge <patrick.njunge@gmail.com>
2026-08-08 16:05:29 +0300

    fix(navigation): default hospitalityEnabled state to true so Hospitality module appears in More sheet

--- androidApp/src/androidMain/kotlin/com/app/biashara/ui/Biashara360App.kt
+++ androidApp/src/androidMain/kotlin/com/app/biashara/ui/Biashara360App.kt
@@ -80,7 +80,7 @@ fun Biashara360App() {
     val navController = rememberNavController()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
-    var hospitalityEnabled by remember { mutableStateOf(false) }
+    var hospitalityEnabled by remember { mutableStateOf(true) }
     var enabledMenus by remember { mutableStateOf<Set<String>?>(null) }
 
     LaunchedEffect(currentDestination?.route) {
